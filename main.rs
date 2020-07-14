b6ea58c6d14cd5b477a4bd035b5da5008cba9334
Aarya <49730497+aaryalamkhade@users.noreply.github.com>
2020-07-14 12:03:43 +0530

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,12 +1,14 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro)] //feature gate which is using unstable features
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket; //using rocket
 
-#[get("/")]
-fn index() -> &'static str {
+#[get("/")] //route attribute
+fn index() -> &'static str //referece with lifetime variable 'static
+{
     "Hello, world!"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index]).launch(); //creating new instace
 }
